600c32134f34516ff7730e0e086eade688adc755
Zefram Lou <zeframlou@gmail.com>
2018-03-05 14:27:37 -0800

    Fixed #3

--- contracts/XY.sol
+++ contracts/XY.sol
@@ -19,6 +19,7 @@ contract XY {
         uint minimumDelay;
         uint epoch;
         address xynotify;
+        uint expireTime;
     }
 
     struct Answer {
@@ -37,18 +38,21 @@ contract XY {
     mapping (address => Answer) public answeredQueries;
     address[] public answerList;
 
-    event QueryReceived(uint xyoValue, address xyoAddress, uint accuracy, uint certainty, uint delay, uint epoch);
+    event QueryReceived(uint xyoValue, address xyoAddress, uint accuracy, uint certainty, uint delay, uint epoch, uint expireTime);
     event AnswerReceived(address xyoAddress, int latitude, int longitude, int altitude, uint accuracy, uint certainty, uint epoch);
 
-    function publishQuery(uint _xyoValue, address _xyoAddress, uint _accuracy, uint _certainty, uint _delay, uint _epoch, address _xynotify) public returns(bool) {
+    function publishQuery(uint _xyoValue, address _xyoAddress, uint _accuracy, uint _certainty, uint _delay, uint _epoch, address _xynotify, uint _expireTime) public returns(bool) {
         require(_xyoValue > 0);
-        pendingQueries[msg.sender] = PendingQuery(_xyoValue, _xyoAddress, _accuracy, _certainty, _delay, _epoch, _xynotify);
-        QueryReceived(_xyoValue, _xyoAddress, _accuracy, _certainty, _delay, _epoch);
+        pendingQueries[msg.sender] = PendingQuery(_xyoValue, _xyoAddress, _accuracy, _certainty, _delay, _epoch, _xynotify, _expireTime);
+        QueryReceived(_xyoValue, _xyoAddress, _accuracy, _certainty, _delay, _epoch, _expireTime);
         return true;
     }
 
     // this is how a diviner sets the answer.  Need to implement the origin proof
     function publishAnswer(address _xyoAddress, int _latitude, int _longitude, int _altitude, uint _accuracy, uint _certainty, uint _epoch) public returns(bool) {
+        if (now > pendingQueries[msg.sender].expireTime) {
+            return false;
+        }
         // TODO: Have to verify before returning
         answeredQueries[_xyoAddress] = Answer(_xyoAddress, _latitude, _longitude, _altitude, _accuracy, _certainty, _epoch);
         answerList.push(_xyoAddress);
@@ -61,7 +65,7 @@ contract XY {
     }
 
     function hasPendingQuery() view public returns(bool) {
-       if(pendingQueries[msg.sender].xyoValue > 0) {
+       if(pendingQueries[msg.sender].xyoValue > 0 && now <= pendingQueries[msg.sender].expireTime) {
            return true;
        }
        return false;
